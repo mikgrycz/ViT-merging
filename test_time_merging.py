7757b053e7d5c6e92409d5f61eca876ba29cd593
Filip Skałka <skalka.fil@gmail.com>
2024-12-05 19:05:04 +0100

    working gradient resolution but some issues with model predictions

--- test_time_merging.py
+++ test_time_merging.py
@@ -1,10 +1,10 @@
 import torch
-from torch import nn, load
+from torch import nn
 from torch.nn import KLDivLoss
 from torchvision import transforms
 from tqdm import tqdm
 
-from datasets.cars import PytorchStanfordCars, Cars
+from datasets.cars import Cars
 from src.task_vectors import TaskVector
 
 
@@ -13,7 +13,6 @@ def freeze_all_but_last_layers(model: nn.Module, n_layers: int = 1):
         param.requires_grad = False
 
     for i in range(1, n_layers + 1):
-        print(f"frozen weight {-i}")
         for pr in model.model.visual.transformer.resblocks[-i].parameters():
             pr.requires_grad = True
 
@@ -26,43 +25,46 @@ def freeze(model: nn.Module):
     return model
 
 
-# TODO: probably not needed
-# class TestTimeMerging(nn.Module):
-#     def __init__(self, pretrained_model, finetuned_model, merged_model, last_layers_update: int = 1):
-#         super(TestTimeMerging, self).__init__()
-#         self.pretrained_model = freeze(pretrained_model)
-#         self.finetuned_model = freeze(finetuned_model)
-#         self.merged_model = freeze_all_but_last_layers(merged_model, n_layers=last_layers_update)
-#         self.last_layers_update = last_layers_update
-#
-#     def forward(self, x):
-#         x_pretrained = self.pretrained_model(x)
-#         x_finetuned = self.finetuned_model(x)
-#         x_merged = self.merged_model(x)
-#         return x_pretrained, x_finetuned, x_merged
+def resolve_gradients(list_grad_loss_p_m, list_grad_loss_f_m):
+    if len(list_grad_loss_p_m) != len(list_grad_loss_f_m):
+        raise ValueError("Lists of tensors must have the same length")
+
+    result_param_list = []
+    for t in range(len(list_grad_loss_p_m)):
+        grad_loss_p_m = list_grad_loss_p_m[t]
+        grad_loss_f_m = list_grad_loss_f_m[t]
+        if grad_loss_p_m.shape != grad_loss_f_m.shape:
+            raise ValueError("Tensors must have the same shape")
+
+        # Case 1: Different signs
+        mask_diff_signs = (grad_loss_p_m * grad_loss_f_m) < 0
+        maxes = torch.max(torch.abs(grad_loss_p_m), torch.abs(grad_loss_f_m))
+
+        p_m_signs = (maxes == torch.abs(grad_loss_p_m)) * torch.sign(grad_loss_p_m)
+        f_m_signs = (maxes == torch.abs(grad_loss_f_m)) * torch.sign(grad_loss_f_m)
+        final_signs = p_m_signs.int() | f_m_signs.int()
+
+        result = maxes * final_signs
+
+        # Case 2: Same signs
+        mask_same_signs = ~mask_diff_signs
+        result[mask_same_signs] = (grad_loss_p_m[mask_same_signs] + grad_loss_f_m[mask_same_signs]) / 2
+
+        result_param_list.append(result)
+
+    return result_param_list
 
 
-def resolve_gradients(grad_loss_p_m, grad_loss_f_m):
-    if grad_loss_p_m.shape != grad_loss_f_m.shape:
-        raise ValueError("Tensors must have the same shape")
-
-    result = torch.empty_like(grad_loss_p_m)
-
-    # Case 1: Different signs
-    mask_diff_signs = (grad_loss_p_m * grad_loss_f_m) < 0
-    result[mask_diff_signs] = torch.max(torch.abs(grad_loss_p_m[mask_diff_signs]), torch.abs(grad_loss_f_m[mask_diff_signs]))
-
-    # Case 2: Same signs
-    mask_same_signs = ~mask_diff_signs
-    result[mask_same_signs] = (grad_loss_p_m[mask_same_signs] + grad_loss_f_m[mask_same_signs]) / 2
-
-    return result
-
-
-
-
-
-
+def log_accuracy(iteration, labels, merged_output, finetuned_output, pretrained_output):
+    _, predicted_merged = torch.max(merged_output, 1)
+    correct_preds_merged = (predicted_merged == labels).sum().item()
+    _, predicted_finetuned = torch.max(finetuned_output, 1)
+    correct_preds_finetuned = (predicted_finetuned == labels).sum().item()
+    _, predicted_pretrained = torch.max(pretrained_output, 1)
+    correct_preds_pretrained = (predicted_pretrained == labels).sum().item()
+    print(f"[MERGED] Correct predictions in iteration {iteration + 1}: {correct_preds_merged}/{labels.size(0)}")
+    print(f"[FINETUNED] Correct predictions in iteration {iteration + 1}: {correct_preds_finetuned}/{labels.size(0)}")
+    print(f"[PRETRAINED] Correct predictions in iteration {iteration + 1}: {correct_preds_pretrained}/{labels.size(0)}")
 
 
 def train(model_p, model_m, model_f, dataloader, optimizer, epochs, n_iterations, device):
@@ -77,35 +79,42 @@ def train(model_p, model_m, model_f, dataloader, optimizer, epochs, n_iterations
         for images, labels in tqdm(dataloader):
             images, labels = images.to(device), labels.to(device)
             for i in range(n_iterations):
-                print(f"iteration {i+1}")
                 optimizer.zero_grad()
 
                 pretrained_output = model_p(images).detach()
                 merged_output = model_m(images)
+
                 loss_p_m = criterion_p_m(merged_output, pretrained_output)
                 loss_p_m.backward(retain_graph=True)
                 # TODO: hardcoded last layer of Transformer
-                # TODO: fix gradient operations (maybe have to work directly on .parameters())
-                grads_loss_p_m = model_m.model.visual.transformer.resblocks[-1].grad.clone()
+                merged_model_last_resblock = model_m.model.visual.transformer.resblocks[-1]
+                p_m_grads = []
+                for param in merged_model_last_resblock.parameters():
+                    p_m_grads.append(param.grad)
 
                 optimizer.zero_grad()
                 finetuned_output = model_f(images).detach()
                 loss_f_m = criterion_f_m(merged_output, finetuned_output)
                 loss_f_m.backward(retain_graph=True)
                 # TODO: hardcoded last layer of Transformer
-                grad_loss_f_m = model_m.model.visual.transformer.resblocks[-1].grad.clone()
+                f_m_grads = []
+                for param in merged_model_last_resblock.parameters():
+                    f_m_grads.append(param.grad)
 
-                resolved_gradients = resolve_gradients(grads_loss_p_m, grad_loss_f_m)
-                # optimizer.zero_grad()
-                model_m.model.visual.transformer.resblocks[-1].grad = resolved_gradients
+                optimizer.zero_grad()
+                resolved_gradients = resolve_gradients(p_m_grads, f_m_grads)
+                for idx, param in enumerate(merged_model_last_resblock.parameters()):
+                    param.grad = resolved_gradients[idx]
 
                 optimizer.step()
 
-                # running_loss += loss.item()
-                #
-                # _, predicted = torch.max(outputs, 1)
-                # total += labels.size(0)
-                # correct += (predicted == labels).sum().item()
+                log_accuracy(i, labels, merged_output, finetuned_output, pretrained_output)
+
+            _, predicted = torch.max(merged_output, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        accuracy = correct / total
+        print(f"Accuracy in epoch {epoch + 1}: {accuracy:.2f}%")
 
 
     return model_p, model_m, model_f
@@ -126,20 +135,9 @@ if __name__ == "__main__":
 
     task_vector = TaskVector(pretrained_path, finetuned_path)
     merged = freeze_all_but_last_layers(task_vector.apply_to(pretrained_path, scaling_coef=0.5))
-    # print(merged.model.visual.transformer.resblocks[-1].requires_grad)
     pretrained = freeze(torch.load(pretrained_path))
     finetuned = freeze(torch.load(finetuned_path))
 
-    # test_time_merging_model = TestTimeMerging(torch.load(pretrained_path), torch.load(finetuned_path), merged)
     optimizer = torch.optim.Adam(merged.model.visual.transformer.resblocks[-1].parameters(), lr=1e-3)
     p, m, f = train(pretrained.to(DEVICE), merged.to(DEVICE), finetuned.to(DEVICE), test_loader, optimizer, 10, 10, DEVICE)
     torch.save(m.merged_model.state_dict(), "checkpoints/first_try.pth")
-
-
-
-
-    # # for block in model.model.visual.transformer.resblocks:
-    # #     print(block)
-    # #     break
-    #
-    # print(model.model.visual.transformer.resblocks[-1])
